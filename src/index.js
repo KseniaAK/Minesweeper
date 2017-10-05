fe59e4489f1b0c318b78629cc96cef625555226f
Ksenia <xushenka@Ksenias-MacBook-Pro.local>
2017-10-04 17:11:10 -0700

    add line for use with chrome redux dev tools

--- src/index.js
+++ src/index.js
@@ -7,6 +7,9 @@ import reducers from './reducers/index'
 
 const store = createStore(reducers)
 
+// To run with Chrome Redux dev tool, run the line below instead
+// const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+
 ReactDOM.render(
   <Provider store={store}>
     <App />
