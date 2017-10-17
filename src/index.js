8cadfba6e67d680630b79d61ce3026afa36fcbee
Ksenia <ksenia.kozh@gmail.com>
2017-10-16 17:35:35 -0700

    add thunk middleware so can dispatch actions from action creators

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducers from './reducers/index'
+import { createStore, applyMiddleware } from 'redux'
+import rootReducer from './reducers/index'
+import thunk from 'redux-thunk'
 
-const store = createStore(reducers)
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 // To run with Chrome Redux dev tool, run the line below instead
 // const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
