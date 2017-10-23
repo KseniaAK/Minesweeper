0da471f6c3f32c1a2305f64477bb3bfe7477b514
Ksenia <ksenia.kozh@gmail.com>
2017-10-23 14:51:09 -0700

    add middleware to the option with chrome extention

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import thunk from 'redux-thunk'
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
 // To run with Chrome Redux dev tool, run the line below instead
-// const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+// const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+//   applyMiddleware(...middleware)
+// ))
 
 ReactDOM.render(
   <Provider store={store}>
