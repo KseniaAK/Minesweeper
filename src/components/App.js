8847f941e2651eca18697dcd6d08ed3abc07c127
Ksenia <ksenia.kozh@gmail.com>
2017-10-23 14:39:06 -0700

    add class to doc body to change its style from react

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import ColorOptions from '../components/ColorOptions'
 import Board from '../containers/Board'
 import NewGameButton from '../containers/NewGameButton'
+import styles from './styles/app.scss'
+import classNames from 'classnames/bind'
+
+const cx = classNames.bind(styles)
 
 export default class App extends Component {
+  componentDidMount() {
+    document.body.classList.add('app-body') 
+  }
+
   render() {
     return (
-      <div className='app'>
+      <div className={cx('app')}>
         <ColorOptions />
         <NewGameButton />
         <Board />
