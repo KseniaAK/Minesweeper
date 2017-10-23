3524c0948f8b0cf755fc13eeb85c0b9c3c9e8d1d
Ksenia <ksenia.kozh@gmail.com>
2017-10-23 14:52:55 -0700

    add state for whether game has started or no

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import ColorReducer from './color_reducer'
 import BoardReducer from './board_reducer'
+import GameReducer from './game_reducer'
 
 const rootReducer = combineReducers({
   color: ColorReducer,
-  boardConfig: BoardReducer
+  boardConfig: BoardReducer,
+  gameOn: GameReducer
 })
 
 export default rootReducer
