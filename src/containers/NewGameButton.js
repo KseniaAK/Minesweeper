9c7af56dc837f43759ffa3858b035ddb2ef942d5
Ksenia <ksenia.kozh@gmail.com>
2017-10-23 15:07:43 -0700

    keep previous board color on new game, do not change to default

--- src/containers/NewGameButton.js
+++ src/containers/NewGameButton.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import { initializeBoard, changeColor } from '../actions/index'
+import styles from './styles/buttons.scss'
 
 class NewGameButton extends Component {
   render() {
@@ -9,8 +10,6 @@ class NewGameButton extends Component {
       <button 
       type='button' 
       onClick={(event) => {
-          event.preventDefault()
-          this.props.changeColor('white')
           this.props.initializeBoard()
         }}
       >
