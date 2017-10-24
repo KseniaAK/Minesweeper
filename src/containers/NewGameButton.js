ae05b471f0254710a88af4c72a2b2ee504bbeafe
Ksenia <ksenia.kozh@gmail.com>
2017-10-24 15:16:33 -0700

    fixed clicking new game button toggles gameon flag to false - cannot blow up on a mine in first click after

--- src/containers/NewGameButton.js
+++ src/containers/NewGameButton.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { initializeBoard, changeColor } from '../actions/index'
+import { initializeBoard, changeColor, gameOver } from '../actions/index'
 import styles from './styles/buttons.scss'
 
 class NewGameButton extends Component {
   render() {
     return (
       <button 
-      type='button' 
+      type='button'
+      className='new-game-button'
       onClick={(event) => {
+          this.props.gameOver()
           this.props.initializeBoard()
         }}
       >
@@ -20,7 +22,7 @@ class NewGameButton extends Component {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ initializeBoard, changeColor }, dispatch)
+  return bindActionCreators({ initializeBoard, changeColor, gameOver }, dispatch)
 }
 
 export default connect(null, mapDispatchToProps)(NewGameButton)
