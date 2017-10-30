35678f600a2f1d47ac6f4c9b342dfe75a46ff0a8
Ksenia <ksenia.kozh@gmail.com>
2017-10-29 18:57:36 -0700

    if player opens a zero - propagate, open all adjacent zero squares

--- src/actions/index.js
+++ src/actions/index.js
@@ -60,11 +60,20 @@ function getMinedSquares() {
 
 export function clickBoardSquare(mouseButton, squareNum) {
   return (dispatch, getState) => {
+    const hasMine = (getState().boardConfig[squareNum - 1].val === 'X')
+    const isZero = (squareNum) => (getState().boardConfig[squareNum - 1].val === 0)
+    const isFlagged = (squareNum) => (getState().boardConfig[squareNum - 1].flag)
+    const isOpen = (squareNum) => (getState().boardConfig[squareNum - 1].open)
+
+    // left mouse button
     if (mouseButton === 0) {
+      // if square is flagged, do not reveal it
+      if (isFlagged(squareNum)) return
+
       // if player opens a mine first click in the game, re-initialize the board
-      if (getState().gameOn === false && getState().boardConfig[squareNum - 1].val === 'X') {
+      if (getState().gameOn === false && hasMine) {
         dispatch(initializeBoard())
-        dispatch(clickBoardSquare(mouseButton, squareNum))
+        return dispatch(clickBoardSquare(mouseButton, squareNum))
       }
       
       if (getState().gameOn === false) dispatch(startGame())
@@ -72,16 +81,29 @@ export function clickBoardSquare(mouseButton, squareNum) {
       // if player clicks with left mouse button, reveal the square
       dispatch(revealSquare(squareNum))
 
+      // if player revealed a zero, propagate and reveal all touching zero squares
+      if (isZero(squareNum)) {
+        const adjacentZeros = getAdjacentZeros(squareNum, WIDTH, getState().boardConfig)
+        adjacentZeros.forEach((square) => {
+          if (isFlagged(square) || isOpen(square)) return
+          else dispatch(clickBoardSquare(0, square))
+        })
+      }
+
       // In case player revealed square is a mine
-      if (getState().boardConfig[squareNum - 1].val === 'X') {
-        // change board color to red
+      if (hasMine) {
+        // change board color to indicate loss
         dispatch(changeColor('gameOver'))
-        // toggle gameon to false to reveal all squares
+        // toggle gameOn to false to reveal all squares
         dispatch(gameOver())
       }
-    } else if (mouseButton === 2) {
-      // if player clicks with right mouse button, flag the square
-      getState().boardConfig[squareNum - 1].flag ? dispatch(unFlagSquare(squareNum)) : dispatch(flagSquare(squareNum))
+    }
+    
+    // right mouse button
+    if (mouseButton === 2) {
+      // flag the square if player clicks with right mouse button for the first time on a given square
+      // UNflag if this is second right-button click on the given square
+      isFlagged(squareNum) ? dispatch(unFlagSquare(squareNum)) : dispatch(flagSquare(squareNum))
     }
   }
 }
@@ -104,4 +126,50 @@ function startGame() {
   return {
     type: START_GAME
   }
+}
+
+function getAdjacentZeros(currSquare, width, allSquaresArray) {
+  const adjacentSquares = [
+    currSquare - width - 1,
+    currSquare - width,
+    currSquare - width + 1,
+    currSquare - 1,
+    currSquare + 1,
+    currSquare + width - 1,
+    currSquare + width,
+    currSquare + width + 1
+  ]
+
+  // Current square is in the first row - no squares above it
+  if (currSquare - width <= 0) {
+    adjacentSquares[0] = null
+    adjacentSquares[1] = null
+    adjacentSquares[2] = null
+  }
+  
+  // Current square is last one in the row - no squares on its right
+  if (currSquare % width === 0) {
+    adjacentSquares[2] = null
+    adjacentSquares[4] = null
+    adjacentSquares[7] = null
+  }
+  
+  // Current square is first one in the row - no square on its left
+  if (currSquare === 1 || currSquare % width === 1) {
+    adjacentSquares[0] = null
+    adjacentSquares[3] = null
+    adjacentSquares[5] = null    
+  }
+  
+  // Current square is in the last row - no squares below it
+  if (currSquare + width > width * width) {
+    adjacentSquares[5] = null
+    adjacentSquares[6] = null
+    adjacentSquares[7] = null
+  }
+
+  return adjacentSquares.filter((square) => {
+    // Check whether adjacent square exists and has zero mines touching it
+    return (square && (allSquaresArray[square - 1].val === 0))
+  })
 }
