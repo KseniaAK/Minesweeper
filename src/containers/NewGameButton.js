83624b0f0035fd4d6593d883b4c6c01f1488ef4a
Ksenia <ksenia.kozh@gmail.com>
2017-10-30 17:38:44 -0700

    delete leftover dispatch mapping that is not being used anymore

--- src/containers/NewGameButton.js
+++ src/containers/NewGameButton.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
-import { initializeBoard, changeColor, gameOver, revertColor } from '../actions/index'
+import { initializeBoard, gameOver, revertColor } from '../actions/index'
 import styles from './styles/buttons.scss'
 
 class NewGameButton extends Component {
@@ -23,7 +23,7 @@ class NewGameButton extends Component {
 }
 
 function mapDispatchToProps(dispatch) {
-  return bindActionCreators({ initializeBoard, changeColor, gameOver, revertColor }, dispatch)
+  return bindActionCreators({ initializeBoard, gameOver, revertColor }, dispatch)
 }
 
 export default connect(null, mapDispatchToProps)(NewGameButton)
