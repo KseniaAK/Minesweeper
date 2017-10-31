3719fe0d6465d7cd2d5d5ef81047e03b9f816df8
Ksenia <ksenia.kozh@gmail.com>
2017-10-30 19:25:04 -0700

    add number of colors as an app-wide constant and import it

--- src/appConstants.js
+++ src/appConstants.js
@@ -1,1 +1,2 @@
 export const WIDTH = 9
+export const NUM_OF_COLORS = 3
--- src/components/ColorOptions.js
+++ src/components/ColorOptions.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react'
+import React from 'react'
 import OneColorOption from '../containers/OneColorOption'
 import styles from './styles/color-options.scss'
 
-export default class ColorOptions extends Component {
-  render() {
-    const NUM_OF_COLORS = 3
-    const colorOptions = []
-    for (let i = 1; i <= NUM_OF_COLORS; i++) {
-      colorOptions.push(<OneColorOption key={i} colorNum={i} />)
-    }
+import { NUM_OF_COLORS } from '../appConstants'
 
-    return (
-      <div className='color-options-main'>
-        <div className='text'>Change the color of your gameboard:</div>
-        <div className='color-options'>
-          {colorOptions}
-        </div>
-      </div>
-    )
+const ColorOptions = () => {
+  const colorOptions = []
+  for (let i = 1; i <= NUM_OF_COLORS; i++) {
+    colorOptions.push(<OneColorOption key={i} colorNum={i} />)
   }
+
+  return (
+    <div className='color-options-main'>
+      <div className='text'>Change the color of your gameboard:</div>
+      <div className='color-options'>
+        {colorOptions}
+      </div>
+    </div>
+  )
 }
+
+export default ColorOptions
