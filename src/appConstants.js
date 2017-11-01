459e35de202673d762f74515e88f8ed46ea98285
Ksenia <ksenia.kozh@gmail.com>
2017-11-01 15:11:10 -0700

    add variable for numbers of mines for suggested difficulty levels

--- src/appConstants.js
+++ src/appConstants.js
@@ -1,2 +1,8 @@
+// width of the gameboard in number of squares
 export const WIDTH = 9
+
+// number of colors to select from for gameboard background color
 export const NUM_OF_COLORS = 3
+
+// pre-suggested number of mines for different difficulty levels
+export const MINE_NUMS = [9, 18, 40]
